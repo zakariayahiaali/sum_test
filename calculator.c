b27b2bc92f76d9268b61eec33e517ad91503384d
Zakaria Yehia <zakaria.yehia@vehiclevo.com>
2024-03-06 22:03:33 +0200

    vdxv

--- calculator.c
+++ calculator.c
@@ -5,15 +5,8 @@ int main() {
     int number1=5;
     int number2=7;
     int sum=0;
-      
 
-
-
-      
- 
-    
-
-    // calculate the sum
+     // calculate the sum
     sum = number1 + number2;      
     
     printf("%d + %d = %d", number1, number2, sum);
