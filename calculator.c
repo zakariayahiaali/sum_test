cc24a4bdcca7b9b5ab24828bf59c37eb266e2111
Zakaria Yehia <zakaria.yehia@vehiclevo.com>
2024-03-06 21:38:24 +0200

    tt

---
+++ calculator.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main() {    
+
+    int number1=5;
+    int number2=7;
+    int sum=0;
+    
+ 
+    
+
+    // calculate the sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
